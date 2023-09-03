a78996b6b1d422785b5a5937714e9cf10dc9ceac
nanja ravelonanosy <romario117910@gmail.com>
2023-09-03 22:25:28 +0200

    version 3 bis pour trigger

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ import java.io.*;
 public class HelloWorld {
     public static void main(String argv[]) {
         System.out.println("Hello World\n");
-        System.out.println("Ceci est une version 3\n");
+        System.out.println("Ceci est une version 3 bis\n");
     }
 }
